0761d3ea285f7647d96ff5832b81feaed69b7190
Carlos Fernandez <cfern1990@gmail.com>
2023-02-23 03:50:56 -0500

    Update prototype token name when imported

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -76,6 +76,9 @@ export class MonsterParser {
                     will: { value: Number(data.will.value) },
                 },
             },
+            prototypeToken: {
+                name: data.name,
+            },
         });
 
         const items: DeepPartial<ItemSourcePF2e>[] = [];
